b9c3ebefe70be3de25a67f726b256a615ad1e846
GONNE <gonne411@gmail.com>
2026-02-27 08:02:31 +0200

    added 1 type hint in a function that didn't have

--- MediaCatalogue.py
+++ MediaCatalogue.py
@@ -28,7 +28,7 @@
 class MediaError(Exception):
     """Custom exception for media-related errors."""
 
-    def __init__(self, message, obj):
+    def __init__(self, message, obj) -> None:
         super().__init__(message)
         self.obj = obj
 
